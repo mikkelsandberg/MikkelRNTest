8596039ae975fb3c14b4cf30bea8ce802270d18c
Mikkel Sandberg <Mikkel.Sandberg@neudesic.com>
2018-10-26 15:45:22 -0700

    implementing react-native-device-info; works except for async stuff

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 export default class App extends Component {
   render() {
+    const DevInfo = target => {
+      if (DeviceInfo[target]() instanceof Promise) {
+        asyncGetInfo();
+      } else {
+        return DeviceInfo[target]();
+      }
+    }
+
+    async function asyncGetInfo() {
+      try {
+        const data = await DeviceInfo[methodToUse]();
+        console.log(data);
+        return data;
+      } catch(err) {
+        console.log(err);
+      }
+    }
+
+    const methodToUse = 'getBatteryLevel';
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>{`${methodToUse} => ${DevInfo(methodToUse)}`}</Text>
       </View>
     );
   }
